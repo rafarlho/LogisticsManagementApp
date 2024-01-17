6e3bc7ade9c8a86ae2c1f728cd29ea50c170908b
rafarlho <rjfcarvalho@ua.pt>
2024-01-17 14:04:55 +0000

    Interface for header complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./auth/login/login.component";
+import { HeaderComponent } from './main-page/header/header.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
-    imports: [CommonModule, RouterOutlet, LoginComponent]
+    imports: [CommonModule, RouterOutlet, LoginComponent,HeaderComponent]
 })
 export class AppComponent {
   title = 'logistics_management_app';
---
+++ src/app/main-page/header/header.component.ts
@@ -0,0 +1,58 @@
+import { DatePipe } from '@angular/common';
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { Subscription, map, share, timer } from 'rxjs';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [
+    MatToolbarModule,
+    DatePipe,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+  ],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+
+  colorHelp:string = "dark"
+  colorProfile:string = "dark"
+  
+  constructor(
+  ) {
+    //let currentDateTime =this.datepipe.transform((new Date), 'MM/dd/yyyy h:mm:ss');
+    //console.log(currentDateTime)
+  }
+
+  //Variables to get real time
+  time:Date =new Date()
+  intervalID!:number
+  subscription!:Subscription
+
+  ngOnInit(): void {
+
+    //Subscription to get real time and update it
+    this.subscription = timer(0,1000)
+      .pipe(
+        map(() => new Date()),
+      )
+      .subscribe(Time => {
+        this.time = Time
+    })
+
+
+  }
+  
+
+
+  ngOnDestroy(): void {
+    clearInterval(this.intervalID) 
+     if(this.subscription) this.subscription.unsubscribe()
+  }
+}
